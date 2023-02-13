f08245f8a16c7ec728049a7b11bfe19d7b6431cf
Marco6666662 <1419699960@qq.com>
2023-02-13 15:19:22 -0500

    update

---
+++ main.js
@@ -0,0 +1,76 @@
+
+// JS File for class demo part 2
+// Xi Chen
+// Last moditifed: 02.07.2023
+
+
+// Event handler for click
+function submitClicked() {
+
+	let vals = document.getElementsByTagName("input");
+
+	console.log(vals.length)
+
+	for (let i = 0; i < vals.length; i++){
+
+
+		if (vals[i].checked){
+			// console.log(vals[i].value);
+
+			// update filler
+			let newText = "Selected Vis: " + vals[i].value;
+			document.getElementById("selected-vis").innerHTML = newText;
+		}
+
+	}
+
+}
+
+
+document.getElementById("subButton")
+		.addEventListener('click', submitClicked);
+
+
+// Anoymous Function and self invoking functions
+(function() {
+	console.log("my anoymous function");
+})();
+
+let note = 'note';
+(function(message) {
+	console.log(message)
+})(note);
+
+(() => {console.log("another anoymous function")})();
+
+((message) => {console.log(message)})(note);
+
+
+// dynamic styling
+function submit2Clicked() {
+
+	let maxPt = document.getElementById("max");
+
+	maxPt.classList.add("orange");
+
+
+}
+
+document.getElementById("subButton2")
+	.addEventListener("click", submit2Clicked);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
